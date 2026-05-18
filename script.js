ba966574d2709b30a42399961d19fcf4343de3d7
K3vin513 <125843375+K3vin513@users.noreply.github.com>
2026-05-17 21:31:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,591 @@
+ var dia = "";
+ var mes = "";
+ var newMes = "";
+ var first = "";
+ var oldFirst = "";
+ var type= "";
+ var typeFruit = "";
+ var value = "";
+ var numFilas = 0;
+ var total = 0;
+ var discountedTotal = 0; 
+ var negativeTest = "false";
+ let register = JSON.parse(
+        localStorage.getItem("registryFiles")
+    ) || [];
+ let registerSaved = JSON.parse(
+        localStorage.getItem("filesSaved")
+    ) || [];
+var resultFinal = 0;
+
+function setType(result, type) {
+
+  if(type == "Tijera"){
+      total = result * 1;
+      return total;
+  }
+
+  if(type == "Mano"){
+      total = result * 0.90;
+      return total;
+  }
+  if(type == "Selección"){
+      total = result * 1.15;
+      return total;
+  }
+
+}
+
+function setQuantity(value, fruit) {
+
+    const ficAlViv = 238.41;
+    const descTra = 67.40;
+
+    if(fruit == "Man.Base"){
+
+       if(value >= 39 && value <= 55 ){
+          result = (value * 22.27) + ficAlViv + descTra;
+         return result;
+       }
+       else if(value < 39){
+            result = 809.82 + ficAlViv + descTra;
+            return result;
+       }
+       else {
+           const sum = value - 55;
+           console.log(sum);
+           result = (sum * 33.4) + (55 * 22.27) + ficAlViv + descTra;
+           return result;
+       }
+    }
+
+
+    if (fruit == "Nar.Base"){
+       if(value <= 55){
+          result = (value * 16.70) + ficAlViv + descTra;
+         return result;
+       }
+       else {
+           const sum = value - 55;
+           result = (sum * 25.05) + (55 * 16.70) + ficAlViv + descTra;
+           return result;
+       }
+    }
+}
+
+function extraHoursValue(initialResult){
+
+   const extraHours = Number(document.getElementById("hours").value);
+    const extraHoursValue = extraHours * 270;
+
+    console.log("Valor de horas extra: " + extraHours);
+
+    discountedTotal = initialResult + extraHoursValue;
+    return discountedTotal;
+}
+
+function discounts(result){
+
+    firstDiscount = result * 0.85;
+    secondDiscount = firstDiscount * 0.97;
+    result = secondDiscount * 0.99;
+    return result;
+
+}
+function callResult(){
+
+    testNegative();
+
+    typeFruit = document.getElementById("fruit").value;
+    value = document.getElementById("quantity").value;
+    type = document.getElementById("type").value;
+
+if(negativeTest == "true"){
+    alert("No hace falta calcular un resultado");
+    return;
+}
+else{
+   if (value === "" || value < 0) {
+    alert("Ingrese un número válido para la cantidad.");
+    return;
+   }
+    const result = setQuantity(value, typeFruit);
+    total = setType(result, type);
+    console.log(result);
+    console.log(type);
+
+    discountedTotal = discounts(total);
+    console.log(discountedTotal);
+
+    const resetQuantity = document.getElementById("quantity");
+    resetQuantity.value = "";
+   
+    const showResult = document.getElementById("showResult");
+    showResult.textContent = "Resultado: $" + total.toFixed(2);
+    return { typeFruit, result, total, type, discountedTotal };
+  }
+}
+
+
+
+function makeMonth(date){
+    
+    const [year, month, day] = date.split("-");
+
+    const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                   "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+    mes = meses[parseInt(month) - 1];
+
+    if(mes == undefined){
+        mes = "";
+    }
+
+    var titleDate = document.getElementById("titleDate");
+    var titleDateContent = titleDate.textContent;
+    console.log("Mes: " + mes);
+
+      if (mes != "" ){
+      // Actualizar el título con el mes
+      titleDate.textContent = mes;
+      newMes = mes;
+      console.log(dia);
+      console.log(first);
+     }
+     else if (mes != newMes && mes != ""){ 
+        
+        titleDateContent = newMes + "-" + mes;
+          console.log(mes);      
+     }
+    localStorage.setItem("mes", mes);
+}
+function makeDay(date){
+
+    const dateObj = new Date(date);
+
+    const days = [ "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"];
+
+    dia = days[dateObj.getDay()];
+
+    return dia;
+}
+function makeNumberDay(date){
+
+    const [year, month, day] = date.split("-");
+
+    first = day;
+    return day;
+}
+
+
+    
+function deleteActions(){
+    
+    const deleteTable = document.getElementById("deleteTable");
+    const renoir = document.getElementById("father");
+    const action = deleteTable.value;
+    const showSaved = document.getElementById("showSaved");
+
+
+    // Si se selecciona "eliminarTodo" se selecciona la tabla, se vacía junto con el titulo de esta y se eliminan todas las listas.
+    if(action === "eliminarTodo"){
+        console.log("Pur la vie");
+        renoir.style.backgroundColor = "CadetBlue";
+
+        const table = document.getElementById("tableFirst");
+        table.style.display = "none";
+        numFilas = 0;
+
+        var titleDate = document.getElementById("titleDate");
+        titleDate.textContent = "";
+        mes = "";
+        newMes = "";
+        first = "";
+        oldFirst = "";
+        localStorage.clear();
+        register = [];
+
+        resultFinal = 0;
+            const pasteResult = document.getElementById("finalResult");
+            pasteResult.textContent = " ";
+            localStorage.removeItem("resultFinal");
+
+        const tableBody = document.getElementById("tBody");
+        while (tableBody.firstChild) {
+            tableBody.removeChild(tableBody.firstChild);
+        }
+    }
+
+    // Si se selecciona "eliminarFila" se verifica si el numero de filas es mayor a uno y si lo es se elimina la última fila, si es
+    // igual a uno entonces se selecciona la tabla, se vacía junto con el título y se elimina la fila.
+    if(action === "eliminarFila"){
+        console.log("Pleurer en couleurs");
+        renoir.style.backgroundColor = "BlanchedAlmond";
+
+        const table = document.getElementById("tBody");
+        if(numFilas > 1){
+            table.deleteRow(-1);
+            numFilas--;
+            console.log("Se elimina la última fila");
+            oldFirst--;
+            register.pop();
+            result = 0;
+            
+            resultFinal = resultFinal - discountedTotal;
+            const pasteResult = document.getElementById("finalResult");
+            pasteResult.textContent = "Total: $" + Math.ceil(resultFinal);
+            localStorage.removeItem("resultFinal");
+
+            localStorage.setItem("first", first);
+            localStorage.setItem("oldFirst", oldFirst);
+            localStorage.setItem("registryFiles", JSON.stringify(register));
+            console.log(resultFinal);
+        }
+        else if (numFilas == 1){
+
+            const table = document.getElementById("tableFirst");
+            table.style.display = "none";
+            numFilas = 0;
+
+            var titleDate = document.getElementById("titleDate");
+            titleDate.textContent = "";
+            mes = "";
+            newMes = "";
+            oldFirst = "";
+            console.log("llega hasta aca");
+            register.pop();
+
+            resultFinal = 0;
+            const pasteResult = document.getElementById("finalResult");
+            pasteResult.textContent = " ";
+            localStorage.removeItem("resultFinal");
+
+
+            const tableBody = document.getElementById("tBody");
+            while (tableBody.firstChild) {
+            tableBody.removeChild(tableBody.firstChild);
+        }
+        }
+    }
+    // Al seleccionar "guardarFila" se muestra un mensaje de guardado y se guarda en el siguiente div
+
+    if(action == "guardarFila"){
+        console.log("Saving file...");
+        saveTable();
+
+        console.log("Pur la vie");
+        renoir.style.backgroundColor = "CadetBlue";
+
+        const table = document.getElementById("tableFirst");
+        table.style.display = "none";
+        numFilas = 0;
+
+        var titleDate = document.getElementById("titleDate");
+        titleDate.textContent = "";
+        mes = "";
+        newMes = "";
+        first = "";
+        oldFirst = "";
+        register = [];
+        localStorage.removeItem("registryFiles");
+
+        resultFinal = 0;
+            const pasteResult = document.getElementById("finalResult");
+            pasteResult.textContent = " ";
+            localStorage.removeItem("resultFinal");
+            
+        const tableBody = document.getElementById("tBody");
+        while (tableBody.firstChild) {
+            tableBody.removeChild(tableBody.firstChild);
+        }
+    
+    }
+    deleteTable.selectedIndex = 0;
+
+}
+
+
+//Al cumplires la condición de que se ha agregado una fila a la tabla, se crea un botón para eliminar la tabla o eliminar la última fila, el cual se muestra al lado del título de la tabla. Este botón se crea una sola vez y se muestra cada vez que se agrega una fila a la tabla, siempre y cuando no haya filas en la tabla. Si el número de filas es mayor a cero entonces el botón se muestra, si el número de filas es igual a cero entonces el botón se oculta.
+function deleteButton(){
+ 
+    if (document.getElementById("deleteTable")) {
+        return;
+    }
+    console.log("Creating delete button...");
+    const deleteTable = document.createElement("select");
+     deleteTable.id = "deleteTable";
+     deleteTable.className = "fontSize";
+     deleteTable.addEventListener("change", deleteActions);
+
+     deleteTable.innerHTML =  `
+                                <option value="" disabled selected hidden>...</option>
+                                <option value="eliminarTodo">Eliminar Tabla</option>
+                                <option value="eliminarFila">Eliminar Ultima Fila</option>
+                                <option value="guardarFila">Guardar Tabla</option>
+                              `; 
+
+    var child = document.getElementById("titleDate");
+    child.appendChild(deleteTable);
+    //Cambiar fondo del div
+    var background = document.getElementById("father1");
+    background.style.backgroundColor = "#F0F8FF";
+}
+
+function testNegative(){
+    negativeTest = document.getElementById("fruit").value;
+}
+
+function getNegative(){
+ 
+    var date = document.getElementById("date").value;
+    var resetDate = document.getElementById("date");
+    const selectFruit = document.getElementById("fruit");
+
+    negativeTest = document.getElementById("fruit").value;
+    console.log(negativeTest);
+    
+    console.log("Negative test is Forgiveness");
+
+    if( date != "" && numFilas == 0){
+     const deleteTable = document.createElement("select");
+     deleteTable.id = "deleteTable";
+     deleteTable.className = "fontSize";
+     deleteTable.addEventListener("change", deleteActions);
+
+     deleteTable.innerHTML =  `
+                                  <option value="" disabled selected hidden>...</option>
+                                  <option value="eliminarTodo">Eliminar Tabla</option>
+                                  <option value="eliminarFila">Eliminar Ultima Fila</option>
+                                  <option value="guardarFila">Guardar Tabla</option>
+
+                             `; 
+
+     var child = document.getElementById("titleDate");
+     child.appendChild(deleteTable);
+   }
+
+    const condition = Math.abs(first - oldFirst) == 1 &&
+                      first > oldFirst || 
+                      oldFirst == "";
+
+    const condition1 = Math.abs(first - oldFirst) > 1 &&
+                       oldFirst != "" 
+                       || first < oldFirst;
+
+    if(date != "" && numFilas <= 5 && condition){
+
+        const tableNegative = document.createElement("tr");
+         tableNegative.innerHTML = `
+                               <tbody>
+                                <th> ${dia} ${first} </th>
+                                <td> Negativo </td>
+                                <td> - </td>
+                                <td> - </td>
+                                <td> $- </td>
+                                <td> $- </td>
+                                </tbody>
+                              `;
+                            
+          var table = document.getElementById("tBody");
+          var showTable = document.getElementById("tableFirst");
+          showTable.style.display = "table";
+          table.appendChild(tableNegative);
+
+          var background = document.getElementById("father1");
+          background.style.backgroundColor = "#F0F8FF";
+
+          oldFirst = first; 
+          localStorage.setItem("first", first);
+          localStorage.setItem("oldFirst", oldFirst);
+          resetDate.value = "";
+          console.log(oldFirst);
+          numFilas++;
+          deleteButton();
+
+        saveArrays();
+        console.log("Daydreaming all the time");
+        console.log(negativeTest);
+        console.log(date.value);
+    }
+
+    else if(condition1){
+        alert("Las fechas no son consecutivas");
+    }
+
+    else if (numFilas > 5){
+        alert("No se pueden agrergar más filas a la tabla");
+        first = "";
+        oldFirst = "";
+    }
+    
+   else if (date == ""){
+    alert("Ingrese una fecha para guardar el día");
+    console.log("Moving on");
+   }
+    selectFruit.selectedIndex = 0;
+  }
+
+  function saveArrays(){
+
+    if(negativeTest == "true"){
+        register.push ({
+            fecha : dia + " " + first,
+            type : "Negativo",
+            typeFruit : "-",
+            value : "-",
+            total : "-",
+            discountedTotal : "-"
+        });
+        localStorage.setItem("registryFiles", JSON.stringify(register));
+        return;
+        }
+            
+    register.push (
+        {
+          fecha : dia + " " + first,
+          type : type,
+          typeFruit : typeFruit,
+          value : value,
+          total : Math.ceil(total),
+          discountedTotal : Math.ceil(discountedTotal)
+        }
+    );
+    localStorage.setItem("first", first);
+    localStorage.setItem("oldFirst", oldFirst);
+    localStorage.setItem("registryFiles", JSON.stringify(register));
+ }
+
+
+  function getTable(){
+
+    const fila = document.createElement("tr");
+    const extraHours = Number(document.getElementById("hours").value);
+
+      fila.innerHTML = `
+       <th></th>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+       <td></td>
+     `;
+
+    var table = document.getElementById("tBody");
+    var showTable = document.getElementById("tableFirst");
+    showTable.style.display = "table";
+    table.appendChild(fila);
+    numFilas++;
+
+    var resetDate = document.getElementById("date");
+    resetDate.value = "";
+    const showResult = document.getElementById("showResult");
+    showResult.textContent = "";
+
+    // Seleccionar los elementos de la tabla por su ID para actualizar su contenido
+    const celdas = fila.children;
+
+    extraHoursValue(discountedTotal);
+
+
+    //Rellenar las celdas con los datos correspondientes.
+    
+        celdas[0].textContent = dia + " " + first;
+        celdas[1].textContent = type;
+        celdas[2].textContent = typeFruit;
+        celdas[3].textContent = value;
+        celdas[4].textContent = "$" + Math.ceil(total);
+        celdas[5].textContent = "$" + Math.ceil(discountedTotal);
+
+    if(extraHours > 0){
+        celdas[5].textContent = "$" + Math.ceil(discountedTotal) + "*";
+    }
+    oldFirst = first;    
+    deleteButton();
+    saveArrays();
+    total = 0;
+    value = "";
+    console.log("Array guardado: " + JSON.stringify(register));
+  }
+
+  function finalResult(afterDiscount){
+
+    resultFinal = resultFinal + afterDiscount;
+    const pasteResult = document.getElementById("finalResult");
+    
+
+    console.log("Asi queda luego del reinicio " + afterDiscount);
+    console.log("Result final: " + resultFinal);
+    pasteResult.textContent = "Total: $" + Math.ceil(resultFinal);
+    total = 0;
+    discountedTotal = 0;
+
+    localStorage.setItem("resultFinal", resultFinal);
+  }
+
+function makeWeek(){
+
+    // Obtener el valor del input de fecha y los datos del mes y día
+    var date = document.getElementById("date").value;
+    const selectExtraHoras = document.getElementById("hours");
+    makeDay(date);
+    makeNumberDay(date);
+    testNegative();
+
+    const condition1 = value != "" 
+                       && dia != undefined 
+                       && mes != undefined 
+                       && numFilas <= 5;
+    const condition = Math.abs(first - oldFirst) == 1 
+                      && first > oldFirst  
+                      || oldFirst =="";
+
+
+
+ if(value != "" || negativeTest == "true"){
+    makeMonth(date);
+}
+
+if(negativeTest == "true"){
+    getNegative();
+    finalResult(discountedTotal);
+    selectExtraHoras.selectedIndex = 0;
+}
+
+else{
+    
+ if(condition1){
+
+   console.log("first condition");
+   console.log(first);
+   console.log(oldFirst);
+
+  // Condicion para crear la tabla, agregar filas a la tabla y crear el botón para eliminar la tabla o eliminar la última fila. La condición se cumple si el número de filas es menor o igual a 5, se ha ingresado un valor para calcular el resultado, se ha seleccionado una fecha y las fechas son consecutivas o no hay fechas anteriores. Si se cumplen estas condiciones entonces se crea la tabla, se agregan filas a la tabla y se crea el botón para eliminar la tabla o eliminar la última fila. Si el número de filas es mayor a 5 entonces se muestra una alerta indicando que no se pueden agregar más filas a la tabla. Si no se ha ingresado un valor para calcular el resultado o no se ha seleccionado una fecha entonces se muestra una alerta indicando que se debe ingresar un valor para calcular el resultado y seleccionar una fecha antes de guardarlo en la tabla. 
+  if(condition){
+    // Crear la fila y agregarla a la tabla
+    getTable();
+    finalResult(discountedTotal);
+    selectExtraHoras.selectedIndex = 0;
+  }
+  else if(Math.abs(first - oldFirst) > 1 && oldFirst != "" || first < oldFirst){
+    alert("Las fechas no son consecutivas");
+        deleteButton();
+
+  }
+  else if(oldFirst == first){
+    alert("Las fechas no son consecutivas");
+        deleteButton();
+
+  }
+  console.log(numFilas);
+
+}
+  else if(value == "" || dia == undefined || mes == undefined){
+    alert("Ingrese una cantidad para calcular el resultado y seleccione una fecha antes de guardarlo en la tabla.");
+  }
+  else if(numFilas > 5){
+    alert("No se pueden agregar más filas a la tabla");
+        deleteButton();
+
+ }
+ }
+} 
---
+++ scriptReload.js
@@ -0,0 +1,185 @@
+let allTables = JSON.parse(localStorage.getItem("allTablesSaved")) || [];
+
+window.onload = function() {
+
+    const select = document.getElementById("selectTable");
+    select.addEventListener("change", showTable);
+
+    const mesGuardado = localStorage.getItem("mes");
+    if (mesGuardado) {
+        document.getElementById("titleDate").innerHTML = mesGuardado;
+    }
+
+    const delButton = localStorage.getItem("deleteButton");
+
+    if (delButton === "true") {
+        console.log("Delete button is true");
+        console.log(delButton);
+        deleteButton();
+    } 
+
+    recreateTable();
+    if(numFilas > 0){
+    first = localStorage.getItem("first");
+    oldFirst = localStorage.getItem("oldFirst");
+    
+    console.log("Luego de crear tabla: ", register);
+    console.log(numFilas);
+    console.log(first);
+    console.log(oldFirst);
+    }
+
+    recreateTableSelect()
+}
+
+
+function saveTable(){
+
+    allTables.push({
+        nombre : localStorage.getItem("mes") + "(" + register[0].fecha + "/" + register[register.length - 1].fecha + ")",
+        mes : localStorage.getItem("mes"),
+        tabla : [...register]
+    })
+    console.log(register[0].fecha);
+    makeTableSelect();
+    register = [];
+    localStorage.setItem("register", JSON.stringify(register)); 
+    localStorage.removeItem("mes");
+    localStorage.removeItem("first");
+    localStorage.removeItem("oldFirst");
+    localStorage.setItem("deleteButton", "false");
+    localStorage.setItem("allTablesSaved", JSON.stringify(allTables));
+}
+
+function makeTableSelect(){
+
+    const select = document.getElementById("selectTable");
+    
+    const lastIndex = allTables.length - 1;
+
+    const lastTable = allTables[lastIndex]; 
+
+    const option = document.createElement("option");
+
+    option.value = lastIndex;
+    option.textContent = lastTable.nombre;
+
+    select.appendChild(option);
+}
+
+function recreateTableSelect(){
+
+    const select = document.getElementById("selectTable");
+
+    // Limpiar opciones anteriores
+    select.innerHTML = `
+        <option value="" disabled selected hidden>
+            Seleccionar tabla
+        </option>
+    `;
+
+    allTables.forEach((table, index) => {
+
+        const option = document.createElement("option");
+
+        option.value = index;
+        option.textContent = table.nombre;
+
+        select.appendChild(option);
+    });
+}
+
+
+function clearTable(){
+
+    var titleDate = document.getElementById("titleSaved");
+        titleDate.textContent = "";
+
+    const tableBody = document.getElementById("tBodySaved");
+
+    while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.firstChild);
+    }
+}
+
+function showTable(event) {
+
+    const selectedIndex = event.target.value;
+    alert("Selected table: " + selectedIndex);
+        recreateSavedTable(selectedIndex);
+}
+
+function recreateWeekSaved(){
+
+       localStorage.setItem("allTablesSaved", JSON.stringify(allTables));
+       allTables = JSON.parse(localStorage.getItem("allTablesSaved")) || [];
+       makeTableSelect();
+}   
+
+function recreateSavedTable(index){
+
+    clearTable();
+
+    const savedTable = allTables[index];
+
+    const titleDate = document.getElementById("titleSaved");
+    var aline = document.getElementById("father2");
+
+    console.log(savedTable);
+    titleDate.innerHTML = savedTable.mes;
+    
+    const tableBody = document.getElementById("tBodySaved");
+
+    savedTable.tabla.forEach(function(item) {
+        
+        const fila = document.createElement("tr");
+
+        fila.innerHTML = `
+        <th>${item.fecha}</th>
+        <td>${item.type}</td>
+        <td>${item.typeFruit}</td>
+        <td>${item.value}</td>
+        <td>$${item.total}</td>
+        <td>$${item.discountedTotal}</td>
+        `;  
+
+    tableBody.appendChild(fila);
+
+    });
+
+    alert("Table saved successfully!");
+    document.getElementById("tableSaved")
+        .style.display = "table";
+    aline.style.backgroundColor = "#dbedfc";
+}
+
+function recreateTable(){
+
+    const tableBody = document.getElementById("tBody");
+    console.log("Recreating table...");
+
+    register.forEach(function(item) {
+        
+        const fila = document.createElement("tr");
+
+        fila.innerHTML = `
+        <th>${item.fecha}</th>
+        <td>${item.type}</td>
+        <td>${item.typeFruit}</td>
+        <td>${item.value}</td>
+        <td>$${item.total}</td>
+        <td>$${item.discountedTotal}</td>
+        `;
+
+        tableBody.appendChild(fila);
+    });
+    if(register.length > 0){
+        deleteButton();
+    }
+    numFilas = register.length;
+    var showTable = document.getElementById("tableFirst");
+    if(numFilas > 0){
+    showTable.style.display = "table";
+       }
+    console.log(numFilas);
+    }
